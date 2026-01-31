d8833920782c8588ca6af7786f141da7b5244959
sam-uit <25410291@ms.uit.edu.vn>
2026-01-31 17:44:11 +0800

    Create SP_AUTH_LOGIN.sql

---
+++ noi-dung/03-cai-dat/SP_AUTH_LOGIN.sql
@@ -0,0 +1,24 @@
+-- SP_AUTH_LOGIN.sql
+CREATE OR ALTER PROCEDURE SP_AUTH_LOGIN
+    @Email NVARCHAR(255),
+    @Password NVARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @InputHash NVARCHAR(255);
+    SET @InputHash = CONVERT(NVARCHAR(255), HASHBYTES('SHA2_256', @Password), 1);
+
+    IF EXISTS (
+        SELECT 1 FROM USERS 
+        WHERE email = @Email AND password_hash = @InputHash
+    )
+    BEGIN
+        SELECT id, email, full_name FROM USERS WHERE email = @Email;
+        PRINT N'Đăng nhập thành công';
+    END
+    ELSE
+    BEGIN
+        RAISERROR(N'Sai email hoặc mật khẩu.', 16, 1);
+    END
+END;
