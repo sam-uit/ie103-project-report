2949aba688f5b0d9270ae288e107196da04d89cc
sam-uit <25410291@ms.uit.edu.vn>
2026-02-01 07:54:34 +0800

    Dữ Liệu Mẫu -- Các Loại Phòng.

---
+++ baocao/content/code/ch03-seed_data-loaiphong.sql
@@ -0,0 +1,13 @@
+INSERT INTO LOAIPHONG (ten_loai, gia_co_ban, mo_ta, suc_chua) VALUES
+(N'Phòng Đơn Tiêu Chuẩn', 500000, N'Phòng đơn cơ bản, giường đơn', 1),
+(N'Phòng Đơn Cao Cấp', 700000, N'Phòng đơn với tiện nghi cao cấp', 1),
+(N'Phòng Đôi Tiêu Chuẩn', 800000, N'Phòng đôi với 2 giường đơn hoặc 1 giường đôi', 2),
+(N'Phòng Đôi Cao Cấp', 1200000, N'Phòng đôi rộng rãi, view đẹp', 2),
+(N'Phòng Gia Đình', 1500000, N'Phòng lớn cho gia đình, 2-3 giường', 4),
+(N'Phòng VIP', 2000000, N'Phòng VIP với đầy đủ tiện nghi', 2),
+(N'Suite Junior', 2500000, N'Suite nhỏ với phòng khách riêng', 2),
+(N'Suite Executive', 3500000, N'Suite cao cấp với nhiều phòng', 4),
+(N'Phòng Deluxe', 1800000, N'Phòng deluxe view biển', 2),
+(N'Phòng Honeymoon', 2200000, N'Phòng trang trí lãng mạn cho cặp đôi', 2);
+
+GO
